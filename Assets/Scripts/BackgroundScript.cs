75fcc4202cbd1f5028f1b8767ea8afd3ebbc49d4
k4sr4 <kasra.j012@gmail.com>
2019-01-26 13:21:37 -0800

    Started adding items

--- Assets/Scripts/BackgroundScript.cs
+++ Assets/Scripts/BackgroundScript.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class BackgroundScript : MonoBehaviour {
 
     public float scrollSpeed;
+    public float timeInterval = 20f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private int division = 0;
+    private float totalTime = 0;
 	
 	// Update is called once per frame
 	void Update ()
     {
         transform.Translate(Vector3.left * Time.deltaTime * scrollSpeed);
+
+        totalTime += Time.deltaTime;
+
+        int newDiv = (int)(totalTime / timeInterval);
+
+        if (newDiv > division)
+        {
+            division = newDiv;
+            scrollSpeed++;
+        }
     }
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,26 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Items
+{
+    Control,
+    Speed,
+    Shield,
+    None
+}
+
 public class PlayerScript : MonoBehaviour {
 
     public float speed;
-    public float power;
-    public int reverse = 1;
     public int id = 1;
+    public float itemTime = 10f;
 
     private float moveHorizontal;
     private float moveVertical;
-    private float origPower;
     private Rigidbody2D rigidBody;
+    private Items activeItem;
+    private float activeItemTime = 0;
+    private float activeItemStartTime = 0;
+
+    private bool shield = false;
+    private bool control = false;
 
     // Use this for initialization
     void Start () {
         rigidBody = GetComponent<Rigidbody2D>();
-        origPower = power;
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+
+    private void Update()
+    {
+        if (activeItem != Items.None)
+        {
+            activeItemTime += Time.deltaTime;
+            if (activeItemTime - activeItemStartTime > itemTime)
+            {
+                control = false;
+                shield = false;
+                activeItem = 0;
+                activeItem = Items.None;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (activeItem == Items.Control)
+            {
+                control = true;
+                activeItemStartTime = Time.deltaTime;
+            }
+            else if (activeItem == Items.Shield)
+            {
+                shield = true;
+                activeItemStartTime = Time.deltaTime;
+            }
+            else if (activeItem == Items.Speed)
+            {
+                activeItemStartTime = Time.deltaTime;
+            }
+        }
+    }
+
+    void FixedUpdate ()
     {
         Move();
 	}
@@ -29,45 +71,57 @@ public class PlayerScript : MonoBehaviour {
     private void Move()
     {
 
-        moveHorizontal = Input.GetAxis("P"+id+" Horizontal");
-        moveVertical = Input.GetAxis("P" + id + " Vertical");
+        //moveHorizontal = Input.GetAxis("P"+id+" Horizontal");
+        //moveVertical = Input.GetAxis("P" + id + " Vertical");
 
-        rigidBody.AddForce(new Vector2(moveHorizontal * speed, moveVertical * speed * reverse));
-       
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
 
-        //float deltaX = moveHorizontal * Time.deltaTime * speed;
-        //float newX = transform.position.x + deltaX;
-        //float deltaY = moveVertical * Time.deltaTime * speed;
-        //float newY = transform.position.y + deltaY;
-
-        //transform.position = new Vector2(newX, newY);
+        if (control)
+        {
+            rigidBody.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+        }
+        else
+        {
+            rigidBody.AddForce(new Vector2(moveHorizontal * speed, moveVertical * speed));
+        }
     }
 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    Collider2D other = collision.collider;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Collider2D other = collision.collider;
 
-    //    if (other.tag == "Player")
-    //    {
-    //        other.GetComponent<PlayerScript>().power = 0;
+        if (other.tag == "Obstacle")
+        {
+            if (shield)
+            {
+                shield = false;
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        else if (other.tag.StartsWith("Target"))
+        {
+            char c = other.tag[other.tag.Length - 1];
+            char i = id.ToString()[0];
 
-    //        Rigidbody2D otherRB = other.gameObject.GetComponent<Rigidbody2D>();
-
-    //        if (otherRB != null)
-    //        {
-    //            print(otherRB);
-    //            otherRB.AddForce(rigidBody.velocity * power);
-    //        }
-    //    }
-    //}
-
-    //private void OnCollisionExit2D(Collision2D collision)
-    //{
-    //    Collider2D other = collision.collider;
-
-    //    if (other.tag == "Player")
-    //    {
-    //        other.GetComponent<PlayerScript>().power = origPower;
-    //    }
-    //}
+            if (other.tag[other.tag.Length - 1] == id.ToString()[0])
+            {
+                print("LAAAAAND!");
+            }
+            else
+            {
+                if (shield)
+                {
+                    shield = false;
+                }
+                else
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+    }
 }
